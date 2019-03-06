166d17904a6b6012b7184f6952d387285235b11a
Egor <egor.burakevich93@gmail.com>
2019-03-06 15:43:53 +0300

    add index

--- task1/main.go
+++ task1/main.go
@@ -2,11 +2,10 @@ package main
 
 // Specify Filter function here
 
-
 func Filter(s []int, fn func(int, int) bool) []int {
-	var p []int 
-	for _, v := range s {
-		if fn(v, 1) {
+	var p []int
+	for i, v := range s {
+		if fn(v, i) {
 			p = append(p, v)
 		}
 	}
