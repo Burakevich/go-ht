07b41043c77d25602c0f0778a45d3a4a548126d5
Egor <egor.burakevich93@gmail.com>
2019-03-06 15:27:33 +0300

    Burakevich Yahor

--- task1/main.go
+++ task1/main.go
@@ -2,6 +2,17 @@ package main
 
 // Specify Filter function here
 
+
+func Filter(s []int, fn func(int, int) bool) []int {
+	var p []int 
+	for _, v := range s {
+		if fn(v, 1) {
+			p = append(p, v)
+		}
+	}
+	return p
+}
+
 func main() {
 
 }
